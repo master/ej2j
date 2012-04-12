470edd57ccf326233aebe7e3053cc8a87fc99998
Oleg Smirnov <oleg.smirnov@gmail.com>
2012-04-12 22:08:08 +0300

    Use new init.d script.

--- debian/init.d
+++ debian/init.d
@@ -6,18 +6,14 @@
 # Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Starts EJ2J transport
-# Description:       Starts MPP-to-XMPP Jabber Transport written in Erlang
 ### END INIT INFO
 
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
+NAME=`basename $0`
+CONF_DIR=/etc/${NAME}
+LOG_DIR=/var/log/${NAME}
 
-INSTANCES="j2j"
-CONF_DIR=/etc/ej2j
-LOG_DIR=/var/log/ej2j/
-LONGNAME="EJ2J"
-COOKIE=SALTIESTWATERS
-APPLICAIONS="alog, crypto, public_key, ssl, exmpp, ej2j"
+test -f /etc/default/${NAME} && . /etc/default/${NAME}
 
 case "$1" in
     start)
@@ -26,13 +22,13 @@ case "$1" in
             if [ -r "${CONF_DIR}/${t}.config" ]
             then
                 echo "Starting ${LONGNAME} instance: ${t}"
-                mkdir -p $LOG_DIR/$NAME
-                run_erl -daemon $LOG_DIR/$NAME $LOG_DIR/$NAME "erl \
+                mkdir -p $LOG_DIR
+                run_erl -daemon $LOG_DIR $LOG_DIR "erl \
 -name ${t}@${hostname} \
--config ${CONF_DIR}/${t} \
+-config ${CONF_DIR}/${t}.config \
 -setcookie ${COOKIE} \
 -boot start_sasl \
--eval '[application:start(A) || A <- [${APPLICAIONS}] ]'"
+-eval '[application:start(A) || A <- [${APPS}] ]'"
             fi
         done
         ;;
@@ -42,8 +38,10 @@ case "$1" in
             pkill -f "${CONF_DIR}/${t}.config"
         done    
         ;;
+    restart)
+        ;;
     *)
-        echo "Usage: $0 {start|stop}" >&2
+        echo "Usage: $0 {start|restart|stop}" >&2
         exit 1
     ;;
 esac
